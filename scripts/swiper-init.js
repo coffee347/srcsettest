63f6e7125cca51856798f3d91b5cae6fc963d5da
coffee347 <49238940+coffee347@users.noreply.github.com>
2024-10-03 19:53:00 +0300

    Add files via upload

--- scripts/swiper-init.js
+++ scripts/swiper-init.js
@@ -1,43 +1,72 @@
 let windowWidth = window.innerWidth;
+
+let ps = new Splide('.ds-row-slider.splide', {
+    arrows: false,
+    classes: {
+        pagination: 'ds-row-slider__pagination',
+    }
+});
+ps.mount();
+
+
+
+
+
+
+
+
+
+
+
 if (windowWidth < 993) {
-    new Swiper('.ds-row-slider', {
-        pagination: {
-            el: '.ds-swiper-pagination',
-        },
-    });
+    // new Swiper('.ds-row-slider', {
+    //     pagination: {
+    //         el: '.ds-swiper-pagination',
+    //     },
+    // });
 }
 
 window.addEventListener('resize', function() {
     if (windowWidth < 993) {
-        let ps = new Swiper('.ds-illustrated-row-ps5 .ds-row-slider', {
-            pagination: {
-                el: '.ds-swiper-pagination',
-            },
-        });
+        // let ps = new Swiper('.ds-illustrated-row-ps5 .ds-row-slider', {
+        //     spaceBetween: 20,
+        //     pagination: {
+        //         el: '.ds-swiper-pagination',
+        //     },
+        // });
 
-        ps.init();
+        // ps.init();
 
-        let iphone = new Swiper('.ds-illustrated-row-iphone .ds-row-slider', {
-            pagination: {
-                el: '.ds-swiper-pagination',
-            },
-        });
+        // let iphone = new Swiper('.ds-illustrated-row-iphone .ds-row-slider', {
+        //     spaceBetween: 20,
+        //     pagination: {
+        //         el: '.ds-swiper-pagination',
+        //     },
+        // });
 
-        iphone.init();
+        // iphone.init();
 
-        let car = new Swiper('.ds-illustrated-row.car .ds-row-slider', {
-            pagination: {
-                el: '.ds-swiper-pagination',
-            },
-        });
-        car.init();        
+        // let car = new Swiper('.ds-illustrated-row.car .ds-row-slider', {
+        //     spaceBetween: 20,
+        //     pagination: {
+        //         el: '.ds-swiper-pagination',
+        //     },
+        // });
+        // car.init();
+
+
     } else {
-        if (document.querySelector('.ds-row-slider').classList.contains('swiper-initialized')) {
-            let allSliders = document.querySelectorAll('.ds-row-slider');
+        // if (document.querySelector('.ds-row-slider').classList.contains('swiper-initialized')) {
+        //     let allSliders = document.querySelectorAll('.ds-row-slider');
 
-            allSliders.forEach(element => {
-                element.swiper.destroy();
-            });
-        }
+        //     allSliders.forEach(element => {
+        //         element.swiper.destroy();
+        //     });
+        // }
+
+
+
     }
 });
+
+
