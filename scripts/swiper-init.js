3a6d05642754283a5e2ecd8c1c0a658e67ab6440
coffee347 <49238940+coffee347@users.noreply.github.com>
2024-10-03 11:37:25 +0300

    Update swiper-init.js

--- scripts/swiper-init.js
+++ scripts/swiper-init.js
@@ -7,33 +7,7 @@ if (windowWidth < 993) {
     });
 }
 
-
-let absolutePositionedEl = document.querySelector('.main-banner-wrapper');
-let mainSection = document.querySelector('.main-banner-wrapper section#main');
-
-let nextSection = document.querySelector('section#need-for-traff');
-
-let problematicElementPosition = '';
-
 window.addEventListener('resize', function() {
-    let windowWidth = window.innerWidth;
-    let windowHeight = window.innerHeight;
-
-    let mainSectionBottomCoord = windowHeight - mainSection.offsetTop - mainSection.offsetHeight;
-    let nextSectionTop = nextSection.offsetTop;
-
-    console.log(mainSectionBottomCoord);
-    console.log(nextSectionTop);
-
-    if (nextSectionTop < mainSectionBottomCoord || nextSectionTop == problematicElementPosition) {
-        problematicElementPosition = nextSectionTop;
-        nextSection.style.marginTop = mainSectionBottomCoord - problematicElementPosition + 10 + 'px';
-    }
-    
-    if (nextSectionTop >= mainSectionBottomCoord + 10) {
-        nextSection.style.marginTop = 0;
-    }
-
     if (windowWidth < 993) {
         let ps = new Swiper('.ds-illustrated-row-ps5 .ds-row-slider', {
             pagination: {
@@ -56,20 +30,14 @@ window.addEventListener('resize', function() {
                 el: '.ds-swiper-pagination',
             },
         });
-        car.init();
-        
-        console.info('Swiper slider initialized');
-        
+        car.init();        
     } else {
         if (document.querySelector('.ds-row-slider').classList.contains('swiper-initialized')) {
             let allSliders = document.querySelectorAll('.ds-row-slider');
 
             allSliders.forEach(element => {
-                element.swiper.destroy();  
-                console.info('Swiper slider deactivated');
+                element.swiper.destroy();
             });
-            
-            
         }
     }
 });
